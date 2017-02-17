6241842d2fb8b032ea62a186db4522edb5f5f78f
Yangyang <ychai@splunk.com>
2017-02-17 15:51:47 +0800

    initial commit

---
+++ src/main/java/yy/test/Processor.java
@@ -0,0 +1,10 @@
+package yy.test;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public class Processor {
+    public static void main(String[] args) {
+        System.out.println("main function");
+    }
+}
